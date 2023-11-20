b497a6458ddf39100c0365514c7573b881b383a4
madhu9515 <139426514+madhu9515@users.noreply.github.com>
2023-11-20 10:54:12 +0530

    Add files via upload

---
+++ multiplication_of_num.py
@@ -0,0 +1,15 @@
+print('MULTIPLICATION OF GIVEN NUM 145')
+n=145
+mul=1
+for a in str(n):
+    c=int(a)
+    mul=mul*c
+print(mul)
+
+print('MULTIPLICATION OF USER INPUT')
+nm=int(input('enter a value = '))
+mull=1
+for i in str(nm):
+    d=int(i)
+    mull=mull*d
+print(mull)
