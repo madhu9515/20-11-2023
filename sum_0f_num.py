a970aa253ed5251d4f6ddf81ef0fd50572ff215d
madhu9515 <139426514+madhu9515@users.noreply.github.com>
2023-11-20 10:49:24 +0530

    Add files via upload

---
+++ sum_0f_num.py
@@ -0,0 +1,17 @@
+print('INPUT IS GIVEN 145')
+n=145
+s=' '
+sm=0
+for a in str(n):
+    c=int(a)
+    sm=sm+c
+print(sm)
+
+
+print('INPUT TAKEN FROM USER')
+m=int(input('enter a value= '))
+ss=0
+for i in str(m):
+    d=int(i)
+    ss=ss+d
+print(ss)
