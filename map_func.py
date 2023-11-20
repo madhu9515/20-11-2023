976bfc4cd1332a38bfd7a11ad712469e0a76c8be
madhu9515 <139426514+madhu9515@users.noreply.github.com>
2023-11-20 11:23:28 +0530

    Add files via upload

---
+++ map_func.py
@@ -0,0 +1,9 @@
+s=(1,2,3,4,5,6)
+x=list(map(lambda a:a*2,s))
+print(x)
+
+
+print('-- map funcation is used to modifiying the data')
+print('-- number of inputs and number of outputs are same')
+print('-- map function takes 2 arguments as inputs')
+print('-- map function prints mapm object')
